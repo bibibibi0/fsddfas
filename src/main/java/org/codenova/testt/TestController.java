e2f3c9bbe732ff6ce8ab05a37bb9097b3b1915a1
hannana0 <index740@gmail.com>
2025-04-11 19:15:16 +0900

    메인생성중

--- src/main/java/org/codenova/testt/TestController.java
+++ src/main/java/org/codenova/testt/TestController.java
@@ -9,12 +9,28 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class TestController {
     @GetMapping("main")
 public String intro() {
+        /*
+        해야 할 것:
+        왼/오른쪽 구역나눠서
+        디테일 페이지 작업
+        마이페이지 작업
+         */
 
         return "test/main";
     }
     @GetMapping("01")
-    public String myPage01() {
+    public String detailPage01() {
 // 페이지 검사 눌러서, 백그라운드 컬러 조정해볼 것
         return "test/01";
+    }
+    @GetMapping("mypage")
+    public String myPage01() {
+   /* 유저 정보 받아온 후
+            프로필 이미지
+              닉네임
+              기타 등등
+            이런 느낌으로 작업
+    */
+        return "test/mypage";
     }
 }
